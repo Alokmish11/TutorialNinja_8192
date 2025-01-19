101f70d856a752d6fb9fb654d3e8590262243a2e
Alokmish11 <140834850+Alokmish11@users.noreply.github.com>
2025-01-19 12:01:28 +0530

    Add files via upload

---
+++ LoginSteps.java
@@ -0,0 +1,47 @@
+package StepDefinitions;
+
+import io.cucumber.java.en.*;
+
+
+public class LoginSteps {
+	
+	@Given("set up")
+	public void set_up() {
+	}
+
+	@When("Registration")
+	public void registration() {
+	   
+	    
+	}
+
+	@When("Login")
+	public void login() {
+	    
+	   
+	}
+
+	@Then("Check Product display")
+	public void check_product_display() {
+	  
+	}
+
+	@Then("CheckOut product")
+	public void check_out_product() {
+	 
+	}
+
+	@Then("check search funtionality")
+	public void check_search_funtionality() {
+	   
+	}
+
+	@Then("quit the driver")
+	public void quit_the_driver() {
+	   
+	}
+
+
+
+
+}
